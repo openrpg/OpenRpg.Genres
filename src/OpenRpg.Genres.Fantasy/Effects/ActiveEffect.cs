25aec9656d9ef8bfc5cd45235ec5889ce3ffebc1
LP <grofit1@yahoo.com>
2020-09-09 13:42:57 +0100

    Removing the temporary effects
    Its been pushed into the combat layer, so can be used in multiple genres now.

--- src/OpenRpg.Genres.Fantasy/Effects/ActiveEffect.cs
+++
@@ -1,9 +0,0 @@
-namespace OpenRpg.Genres.Fantasy.Effects
-{
-    public class ActiveEffect
-    {
-        public TemporaryEffect Effect { get; set; }
-        public float ActiveTime { get; set; }
-        public float TimeSinceProc { get; set; }
-    }
-}
