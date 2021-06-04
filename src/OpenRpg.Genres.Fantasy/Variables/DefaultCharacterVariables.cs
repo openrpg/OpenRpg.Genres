758e76e5709cc76475571bb564121faa4e0c95bc
LP <grofit1@yahoo.com>
2021-06-04 15:30:09 +0100

    Bubbled up constructors for variables

--- src/OpenRpg.Genres.Fantasy/Variables/DefaultCharacterVariables.cs
+++ src/OpenRpg.Genres.Fantasy/Variables/DefaultCharacterVariables.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using OpenRpg.Core.Variables;
 
 namespace OpenRpg.Genres.Fantasy.Variables
 {
     public class DefaultCharacterVariables : DefaultVariables<object>, ICharacterVariables
-    {}
+    {
+        public DefaultCharacterVariables(IDictionary<int, object> internalVariables = null) : base(internalVariables)
+        {
+        }
+    }
 }
