02fed41f90ab4a67edaa458dd82fef7804ecebf3
LP <grofit1@yahoo.com>
2021-07-12 15:09:14 +0100

    This breaking change removes the randomness from the attack generator
    This can be added back in through the random modifier provided through combat core now.

--- src/OpenRpg.Genres.Fantasy/Combat/BasicAttackGenerator.cs
+++ src/OpenRpg.Genres.Fantasy/Combat/BasicAttackGenerator.cs
@@ -1,36 +1,19 @@
-using System.Collections.Generic;
 using System.Linq;
 using OpenRpg.Combat.Attacks;
 using OpenRpg.Combat.Processors;
 using OpenRpg.Core.Stats;
-using OpenRpg.Core.Utils;
 using OpenRpg.Genres.Fantasy.Extensions;
 
 namespace OpenRpg.Genres.Fantasy.Combat
 {
     public class BasicAttackGenerator : IAttackGenerator
     {
-        public IRandomizer Randomizer { get; }
-
-        public BasicAttackGenerator(IRandomizer randomizer)
-        {
-            Randomizer = randomizer;
-        }
-
-        public float GenerateRandomFrom(float maximumValue, float startFrom = 0.75f)
-        { return Randomizer.Random(maximumValue * startFrom, maximumValue); }
-
         public Attack GenerateAttack(IStatsVariables stats)
         {
-            var damages = new List<Damage>();
-            var applicableDamages = stats.GetDamageReferences().Where(x => x.StatValue != 0);
-
-            foreach (var applicableDamage in applicableDamages)
-            {
-                var actualDamage = GenerateRandomFrom(applicableDamage.StatValue);
-                var newDamage = new Damage(applicableDamage.StatType, actualDamage);
-                damages.Add(newDamage);
-            }
+            var damages = stats.GetDamageReferences()
+                .Where(x => x.StatValue != 0)
+                .Select(x => new Damage(x.StatType, x.StatValue))
+                .ToArray();
 
             return new Attack(damages);
         }
