3e6024bd3b741685ef1661c2db1a254524a899c3
LP <grofit1@yahoo.com>
2021-08-22 14:20:13 +0100

    Added default character

---
+++ src/OpenRpg.Genres/Characters/DefaultCharacter.cs
@@ -0,0 +1,24 @@
+﻿using OpenRpg.Core.Classes;
+using OpenRpg.Core.Races;
+using OpenRpg.Core.Stats;
+using OpenRpg.Genres.Variables;
+using OpenRpg.Items.Equipment;
+
+namespace OpenRpg.Genres.Characters
+{
+    /// <summary>
+    /// A default character implementation, this can be ignored or used as a basis for your own
+    /// </summary>
+    public class DefaultCharacter : ICharacter
+    {
+        public int Id { get; set; }
+        public string NameLocaleId { get; set; }
+        public string DescriptionLocaleId { get; set; }
+        public IStatsVariables Stats { get; set; }
+        public IClass Class { get; set; }
+        public byte GenderType { get; set; }
+        public IRaceTemplate Race { get; set; } = new DefaultRaceTemplate();
+        public IEquipment Equipment { get; set; } = new DefaultEquipment();
+        public ICharacterVariables Variables { get; set; } = new DefaultCharacterVariables();
+    }
+}
