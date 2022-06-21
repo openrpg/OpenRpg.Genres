cfd46d7913a962db7eed051c3f87d19ee2771a8c
LP <grofit1@yahoo.com>
2022-06-21 17:11:37 +0100

    Updated missing link for objective types

--- src/OpenRpg.Genres/Types/ObjectiveTypes.cs
+++ src/OpenRpg.Genres/Types/ObjectiveTypes.cs
@@ -1,6 +1,6 @@
 namespace OpenRpg.Genres.Types
 {
-    public interface ObjectiveTypes
+    public interface ObjectiveTypes : Quests.Types.ObjectiveTypes
     {
         public static int CurrencyObjective = 30;
         public static int EnemyDefeatedObjective = 31;
